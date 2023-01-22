1128fa179e52f34056433ad40bf4109b7733aa5c
alfa-th <itsalfath123@gmail.com>
2023-01-22 19:27:27 +0700

    chore: moving and renaming files

--- week1/dock_sql/postgres.sh
+++
@@ -1,9 +0,0 @@
-docker run -it \
-    -e POSTGRES_USER="root" \
-    -e POSTGRES_PASSWORD="root" \
-    -e POSTGRES_DB="ny_taxi" \
-    -v /home/user/de-zoomcamp-2023/week1/dock_sql/ny_taxi_postgres_data:/var/lib/postgresql/data \
-    -p 5432:5432 \
-    postgres:13
-
-    
